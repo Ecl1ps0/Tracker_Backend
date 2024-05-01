cc91452afae8afefb95f815bbcc259656e0699d2
Ecl1ps0 <93914852+Ecl1ps0@users.noreply.github.com>
2024-05-01 19:54:39 +0500

    feat: get id form the context

--- pkg/handler/middleware.go
+++ pkg/handler/middleware.go
@@ -1,6 +1,8 @@
 package handler
 
 import (
+	"errors"
+	"fmt"
 	"github.com/gin-gonic/gin"
 	"net/http"
 	"strings"
@@ -10,13 +12,14 @@ const authHeader = "Authorization"
 
 func (h *Handler) userIdentity(c *gin.Context) {
 	header := c.GetHeader(authHeader)
-	if header != "" {
+	if header == "" {
 		newErrorResponse(c, http.StatusUnauthorized, "Authorization header is empty!")
 		return
 	}
 
 	headerParts := strings.Split(header, " ")
-	if len(headerParts) != 2 {
+	fmt.Println(headerParts)
+	if len(headerParts) != 2 || headerParts[0] != "Bearer" {
 		newErrorResponse(c, http.StatusUnauthorized, "Invalid authorization header!")
 		return
 	}
@@ -28,4 +31,18 @@ func (h *Handler) userIdentity(c *gin.Context) {
 	}
 
 	c.Set("userId", userId)
+}
+
+func getUserId(c *gin.Context) (uint, error) {
+	id, ok := c.Get("userId")
+	if !ok {
+		return 0, errors.New("User id not found")
+	}
+
+	idInt, ok := id.(uint)
+	if !ok {
+		return 0, errors.New("user id is of invalid type")
+	}
+
+	return idInt, nil
 }
