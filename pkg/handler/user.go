ed3cf060da4136ec58589b65cc8d7822ab1a1999
Ecl1ps0 <93914852+Ecl1ps0@users.noreply.github.com>
2024-05-04 00:15:54 +0500

    upd: user for add to task endpoint

--- pkg/handler/user.go
+++ pkg/handler/user.go
@@ -3,6 +3,7 @@ package handler
 import (
 	"github.com/gin-gonic/gin"
 	"net/http"
+	"strconv"
 )
 
 func (h *Handler) getProfile(c *gin.Context) {
@@ -19,4 +20,48 @@ func (h *Handler) getProfile(c *gin.Context) {
 	}
 
 	c.JSON(http.StatusOK, user)
+}
+
+func (h *Handler) addStudentToTask(c *gin.Context) {
+	userId, err := getUserId(c)
+	if err != nil {
+		newErrorResponse(c, http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	roleId, err := h.service.User.GetRoleByUserID(userId)
+	if err != nil {
+		newErrorResponse(c, http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	taskId, err := strconv.Atoi(c.Param("taskID"))
+	if err != nil {
+		newErrorResponse(c, http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	task, err := h.service.Task.GetTaskByID(uint(taskId))
+	if err != nil {
+		newErrorResponse(c, http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	if task.TeacherID != userId && roleId != 3 {
+		newErrorResponse(c, http.StatusForbidden, "Only Teacher created task and Admin can add task to student task!")
+		return
+	}
+
+	studentId, err := strconv.Atoi(c.Param("studentID"))
+	if err != nil {
+		newErrorResponse(c, http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	if err := h.service.User.AddStudentToTask(uint(studentId), uint(taskId)); err != nil {
+		newErrorResponse(c, http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	c.Status(http.StatusOK)
 }
