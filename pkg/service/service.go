59a745a51e46aecafcfe6ebab5a369298daeb4a6
Ecl1ps0 <93914852+Ecl1ps0@users.noreply.github.com>
2024-05-07 18:26:43 +0500

    upd: get students tasks, change Task model to StudentTask model

--- pkg/service/service.go
+++ pkg/service/service.go
@@ -30,7 +30,7 @@ type Task interface {
 	GetAllTasks() ([]models.Task, error)
 	GetTaskByID(id uint) (models.Task, error)
 	GetAllTeacherTasks(id uint) ([]models.Task, error)
-	GetAllStudentTasks(id uint) ([]models.Task, error)
+	GetAllStudentTasks(id uint) ([]models.StudentTask, error)
 	UpdateTask(task models.Task) error
 	DeleteTask(id uint) error
 }
