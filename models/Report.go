08f2a53fefd0bb9f53e2ae7ed4f8a090abf49e7b
Ecl1ps0 <93914852+Ecl1ps0@users.noreply.github.com>
2024-05-11 00:59:45 +0500

    feat: add ReportID to Solution

--- models/Report.go
+++ models/Report.go
@@ -4,9 +4,7 @@ import "gorm.io/gorm"
 
 type Report struct {
 	gorm.Model
-	Logs              []byte `binding:"required"`
-	Report            []byte `binding:"required"`
-	Clipboard         []byte `binding:"required"`
-	StudentSolutionID uint
-	StudentSolution   *StudentSolution `gorm:"foreignKey:StudentSolutionID"`
+	Logs      []byte `binding:"required"`
+	Report    []byte `binding:"required"`
+	Clipboard []byte `binding:"required"`
 }
